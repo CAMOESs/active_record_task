dec23972b20ff6081b7634a38dd21324bc239926
camoes <cam@camoes.fr>
2022-10-02 08:02:42 +0100

    coommit

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,7 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food.left_outer_joins(:order_foods)
+    @foods = Food.left_outer_joins(:orders)..where(orders: { customer_id: " "})
   end
 
   def exercise2
